54646bcb94348161d3c5f41ea8d0674d172da3d1
Denis Levinskiy <levinskiy.d@gmail.com>
2020-12-13 16:16:39 +0300

    ver. 1.1

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -1,56 +1,72 @@
 public class Restaurant {
 
-    int visitorsWantToOrder;
-    int ordersCooked;
-    int ordersDone;
+    int visitorsWantToOrder = 0;
+    int ordersCooked = 0;
+
+    int totalNumberOfVisitorsEntered = 0;
+    int totalNumberOfOrdersAccepted = 0;
+    int totalNumberOfOrdersDone = 0;
 
     final int VISITORSTIMETOCHOOSE = 5000;
     final int TIMETOCOOK = 7000;
     final int VISITORSTIMETOEAT = 3000;
+    final int LIMIT = 5;
 
-    public synchronized void bringFood() {
+    public void bringFood() {
 
         System.out.println(Thread.currentThread().getName() + " на работе");
 
-        while (ordersDone < 5) {
+        while (totalNumberOfOrdersDone < LIMIT) {
 
-            while (visitorsWantToOrder == 0) {
-                try {
-                    if (ordersDone == 5) return;
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            synchronized (this) {
+                while (visitorsWantToOrder == 0) {
+
+                    if (totalNumberOfOrdersAccepted >= LIMIT) {
+                        System.out.println(Thread.currentThread().getName() + " закончил работу");
+                        return;
+                    }
+
+                    try {
+                        System.out.println(Thread.currentThread().getName() + " ждет заказов посетителей");
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
+
+                System.out.println(Thread.currentThread().getName() + " принял заказ");
+                visitorsWantToOrder--;
+                totalNumberOfOrdersAccepted++;
             }
 
-            System.out.println(Thread.currentThread().getName() + " принял заказ");
-            visitorsWantToOrder -= 1;
-
+            System.out.println("Заказ готовится...");
             try {
-                System.out.println("Заказ готовится...");
                 Thread.sleep(TIMETOCOOK);
-                ordersCooked += 1;
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
-            System.out.println(Thread.currentThread().getName() + " принес заказ");
-            ordersDone += 1;
-
-            notifyAll();
+            synchronized (this) {
+                ordersCooked++;
+                System.out.println(Thread.currentThread().getName() + " принес заказ");
+                totalNumberOfOrdersDone++;
+                notifyAll();
+            }
         }
-        System.out.println("Ресторан закрывается...");
+
+        System.out.println(Thread.currentThread().getName() + " закончил работу");
     }
 
 
-    public synchronized void orderFood() {
+    public void orderFood() {
 
-        if (ordersDone >= 5) {
+        if (totalNumberOfVisitorsEntered >= LIMIT) {
             System.out.println(Thread.currentThread().getName() + ", ресторан закрыт");
             return;
         }
 
         System.out.println(Thread.currentThread().getName() + " зашел в ресторан");
+        totalNumberOfVisitorsEntered++;
 
         try {
             Thread.sleep(VISITORSTIMETOCHOOSE);
@@ -58,26 +74,31 @@ public class Restaurant {
             e.printStackTrace();
         }
 
-        visitorsWantToOrder += 1;
-        System.out.println(Thread.currentThread().getName() + " готов заказать");
-        notifyAll();
-
-        while (ordersCooked == 0) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        synchronized (this) {
+            visitorsWantToOrder++;
+            System.out.println(Thread.currentThread().getName() + " готов заказать");
+            notifyAll();
         }
 
-        ordersCooked -= 1;
+        synchronized (this) {
+            while (ordersCooked == 0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            ordersCooked--;
+        }
+
         System.out.println(Thread.currentThread().getName() + " ест заказ");
+
         try {
             Thread.sleep(VISITORSTIMETOEAT);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(Thread.currentThread().getName() + " уходит");
-        notifyAll();
+
+        System.out.println(Thread.currentThread().getName() + " уходит. Чао!");
     }
 }
